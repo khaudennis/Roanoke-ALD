b193682ae78c57ab6d4b2f8988cf9938204005bc
Dennis Khau <iam@denniskhau.com>
2019-05-21 21:00:34 -0400

    Initial commit of archive.

---
+++ index.php
@@ -0,0 +1,176 @@
+<html>
+<head>
+<title>Roanoke College Chapter of Alpha Lambda Delta | Salem, VA | Official Site</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-color: #FEC107;
+}
+-->
+</style>
+<link href="css/dkhau_style_s_1.css" rel="stylesheet" type="text/css">
+</head>
+<body> 
+<table border="0" cellpadding="0" cellspacing="0" width="800"> 
+  <!-- fwtable fwsrc="dkhau_ald_rc_web_design_1_v1b1_1.png" fwbase="dk_ald_web_slices_1_v1b1_1_" fwstyle="Dreamweaver" fwdocid = "1578482466" fwnested="0" --> 
+  <tr> 
+    <td><img src="images/spacer.gif" width="17" height="1" border="0" alt=""></td> 
+    <td><img src="images/spacer.gif" width="367" height="1" border="0" alt=""></td> 
+    <td><img src="images/spacer.gif" width="221" height="1" border="0" alt=""></td> 
+    <td><img src="images/spacer.gif" width="66" height="1" border="0" alt=""></td> 
+    <td><img src="images/spacer.gif" width="59" height="1" border="0" alt=""></td> 
+    <td><img src="images/spacer.gif" width="270" height="1" border="0" alt=""></td> 
+  </tr> 
+    <td colspan="2" background="images/dk_ald_web_slices_1_v1b1_1__r1_c4.gif"><div align="right"><img name="dk_ald_web_slices_1_v1b1_1__r1_c1" src="images/dk_ald_web_slices_1_v1b1_1__r1_c1.gif" width="384" height="217" border="0" alt="Alpha Lambda Delta"></div></td> 
+    <td background="images/dk_ald_web_slices_1_v1b1_1__r1_c4.gif"><img name="dk_ald_web_slices_1_v1b1_1__r1_c3" src="images/dk_ald_web_slices_1_v1b1_1__r1_c3.gif" width="221" height="217" border="0" alt="Alpha Lambda Delta"></td> 
+    <td colspan="3" background="images/dk_ald_web_slices_1_v1b1_1__r1_c5.gif">&nbsp;</td> 
+  </tr> 
+
+    <tr background="images/dk_ald_web_slices_1_v1b1_1__r2_c3.gif"><td colspan="6" background="images/dk_ald_web_slices_1_v1b1_1__r2_c3.gif"><img name="dk_ald_web_slices_1_v1b1_1__r2_c3" src="images/dk_ald_web_slices_1_v1b1_1__r2_c3.gif" width="221" height="23" border="0" alt=""></td> 
+  </tr> 
+  <tr bgcolor="#FFFFFF"> 
+    <td colspan="6"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="19%"><table width="100%"  border="0" cellpadding="1" cellspacing="1">
+            <tr>
+              <td><strong>Site Navigation</strong>
+                <table width="100%"  border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home">Home</a></td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home"></a><a href="index.php?p=about">About</a></td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home"></a><a href="index.php?p=membership">Membership</a></td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home"></a><a href="index.php?p=opportunities">Opportunities</a></td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home"></a><a href="index.php?p=events">Events</a></td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home"></a><a href="index.php?p=officers">Officers</a></td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home"></a><a href="index.php?p=links">Links</a></td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#FEC108"><img src="images/r_bullet.png" width="10" height="10" align="absmiddle"> <a href="index.php?p=home"></a><a href="index.php?p=contact_us">Contact Us</a> </td>
+                  </tr>
+                </table></td>
+            </tr>
+            <tr>
+              <td><strong>Upcoming Events</strong>                <table width="100%"  border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+                  <tr>
+                    <td bgcolor="#FEC108">No upcoming events.</td>
+                  </tr>
+                </table> </td>
+            </tr>
+            <tr>
+              <td><strong>Contact Information</strong>                <table width="100%"  border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+                  <tr>
+                    <td bgcolor="#FEC108">Roanoke College<br>
+                      c/o Alpha Lambda Delta<br>
+                      221 College Lane<br>
+                      Salem, Virginia 24153</td>
+                  </tr>
+                </table></td>
+            </tr>
+            <tr>
+              <td><div align="center"><a href="http://www.roanoke.edu/" target="_blank"><img src="images/rc_std_logo_175_99.jpg" alt="Roanoke College" width="175" height="99" border="0"></a></div></td>
+            </tr>
+        </table></td>
+        <td width="81%" align="left" valign="top"><table width="100%"  border="0" cellspacing="5" cellpadding="3">
+            <tr>
+              <td><div align="center">
+                <img src="images/g_bullet.png" width="10" height="10"> 
+                <?php
+// BODY DISPLAYING PAGES
+// SET VARIABLES
+$page = $_GET['p']; 
+
+// Home, Index Page
+if (($page == "home") or ($page == "")) {
+include('heading/index.htm'); 
+} 
+
+// Navigational Links - Primary
+  else if($page == "about") { 
+include('heading/about.htm'); 
+} else if($page == "membership") { 
+include('heading/membership.htm'); 
+} else if($page == "opportunities"){ 
+include('heading/opportunities.htm');
+} else if($page == "events"){ 
+include('heading/events.htm');
+} else if($page == "officers"){ 
+include('heading/officers.htm');
+} else if($page == "links"){ 
+include('heading/links.htm');
+} else if($page == "contact_us"){ 
+include('heading/contact_us.htm');
+}
+
+?>
+                 <img src="images/g_bullet.png" width="10" height="10"> </div></td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td align="left" valign="top"><?php
+// BODY DISPLAYING PAGES
+// SET VARIABLES
+$page = $_GET['p']; 
+
+// Home, Index Page
+if (($page == "home") or ($page == "")) {
+include('contents/index.htm'); 
+} 
+
+// Navigational Links - Primary
+  else if($page == "about") { 
+include('contents/about.htm'); 
+} else if($page == "membership") { 
+include('contents/membership.htm'); 
+} else if($page == "opportunities"){ 
+include('contents/opportunities.htm');
+} else if($page == "events"){ 
+include('contents/events.htm');
+} else if($page == "officers"){ 
+include('contents/officers.htm');
+} else if($page == "links"){ 
+include('contents/links.htm');
+} else if($page == "contact_us"){ 
+include('contents/contact_us.htm');
+}
+
+?></td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr> 
+
+    <tr background="images/dk_ald_web_slices_1_v1b1_1__r4_c2.gif"><td colspan="5" valign="baseline" background="images/dk_ald_web_slices_1_v1b1_1__r4_c2.gif" class="Copyright"><table width="100%"  border="0" cellspacing="1" cellpadding="1"> 
+        <tr> 
+          <td class="Copyright"><a href="index.php?p=home">Home</a> | <a href="index.php?p=about">About</a> | <a href="index.php?p=membership">Membership</a> | <a href="index.php?p=opportunities">Opportunities</a> | <a href="index.php?p=events">Events</a> | <a href="index.php?p=officers">Officers</a> | <a href="index.php?p=links">Links</a> | <a href="index.php?p=contact_us">Contact Us</a> <br> 
+            Alpha Lambda Delta at Roanoke College; Salem, Virginia 24153 <br> 
+            Questions, comments, suggestions? <a href="index.php?p=contact_us">Please contact us!</a> </td> 
+        </tr> 
+    </table></td> 
+    <td background="images/dk_ald_web_slices_1_v1b1_1__r4_c2.gif"><div align="right"><img name="dk_ald_web_slices_1_v1b1_1__r4_c6" src="images/dk_ald_web_slices_1_v1b1_1__r4_c6.gif" width="270" height="90" border="0" alt="Alpha Lambda Delta"></div></td> 
+  </tr> 
+</table> 
+</body>
+</html>
